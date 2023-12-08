3778fbb39986b95c70d3caca3c67849c57b2e57b
rskoupy <skoupy.radim@gmail.com>
2023-12-08 10:24:18 +0100

    Secom detector reading added

--- src/stemdiff/detectors.py
+++ src/stemdiff/detectors.py
@@ -21,6 +21,7 @@ Therefore, the new classes = new detectors can be added quite easily:
 import sys
 import inspect
 import numpy as np
+from PIL import Image
     
 def list_of_known_detectors():
     '''
@@ -140,29 +141,20 @@ class TimePix:
 
 class Secom:
     
-    # TODO: Radim
-    # Zkontroluj, jestli je trida aktualni.
-    # Mozna se neco pokazilo pri aktualizacich?
-    # V tuto chvili to mozna neni uplne dokonceno:
-    #  - detector_name='TimePix' => asi by melo byt 'Secom'
-    #  - a jinak to vypada, ze to je stejne jako TimePix, je aktualizovano?
-    
     '''
     Definition of Secom detector.
     
     Parameters
     ----------
-    detector_name : str, default is 'TimePix'
+    detector_name : str, default is 'Secom'
         Name of the detector.
         Keep the default unless you have specific reasons.
-    detector_size : integer, default is 256
+    detector_size : integer, default is 2048
         Size of the detector in pixels.
         Keep the default unless you have specific reasons.
-    data_type : numpy data type, optional, default is np.uint16
-        Type of data, which are saved in the binary file.
-        TimePix detector saves the data as 16-bit integers.
-        This corresponds to np.uint16 (more info in NumPy documentation).
-    upscale : integer, default is 4
+    data_type : tiff files
+        
+    upscale : integer, default is 1
         Upscaling coefficient.
         Final image size = detector_size * upscale.
         The upscaling coefficient increases the detector resolution.
@@ -170,59 +162,47 @@ class Secom:
     
     Returns
     -------
-    TimePix detector object.
+    Secom detector object.
     '''
     
-    def __init__(self,
-                 detector_name='TimePix', 
-                 detector_size=256, data_type=np.uint16, upscale=4):
+    def __init__(self, detector_name='Secom', 
+                 detector_size=2048, max_intensity=65536,
+                 data_type=np.uint16, upscale=1):
         '''
-        Initialize parameters of TimePix detector.
+        Initialize parameters of Secom detector.
         The parameters are described above in class definition.
         '''
         self.detector_name = detector_name
         self.detector_size = detector_size
+        self.max_intensity = max_intensity
         self.data_type = data_type
         self.upscale = upscale
     
     def read_datafile(self, filename, arr_size=None):
         '''
-        Read datafile in TimePix detector format.
+        Read datafile in Secom detector format.
 
         Parameters
         ----------
         filename : str or path
             Name of the datafile to read.
-        arr_size : int, optional, default is None
-            Size of the square array to reade.
-            Typically, we read original datafiles with size = detector.size.
-            Nonetheless, we can read saved also datafiles with size = arr_size.
 
         Returns
         -------
         arr : 2D-numpy array
-            2D-array containing image from TimePix detector.
+            2D-array containing image from Secom detector.
             Each element of the array = the intensity detected at given pixel.
         '''
-        # Read binary datafile (to 1D-array)
-        arr = np.fromfile(filename, dtype=self.data_type)
-        # Determine edge of the file - we work only with square files
-        edge = int(np.sqrt(arr.size))
-        # Reshape the array and return
-        arr = arr.reshape(edge, edge)
+        
+        arr = np.array(Image.open(filename)) 
         return(arr)
     
     def save_datafile(self, arr, filename):
         '''
-        Save 2D-array as a datafile in the TimePix detector format.
+        Save 2D-array as a datafile in the Secom detector format.
         '''
-        # Slightly modified according to
-        # https://stackoverflow.com/q/43211616
-        fh = open(filename,'wb')
-        arr = arr.flatten()
-        BlockArray = np.array(arr).astype(np.uint16)
-        BlockArray.tofile(fh)
-        fh.close()
+        im = Image.fromarray(arr.astype(np.uint16))
+        im.save(filename)
 
 
 class Arina:
